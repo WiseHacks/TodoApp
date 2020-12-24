e37bcbc3560837a87b7838464d9b7edddc0e7626
Divyesh <iit2019063@iiita.ac.in>
2020-12-24 16:32:31 +0530

    initial build

--- app/src/main/java/com/example/testingkotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/testingkotlinapp/MainActivity.kt
@@ -6,34 +6,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var todoAdapter : CustomAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        btnPress.setOnClickListener{
-            Log.d("LOG","pressed")
-            Toast.makeText(this, "pressed", Toast.LENGTH_SHORT).show()
-            val message = edtText.text.toString()
-            Log.d("check","edtTxt : $message")
-            val intent = Intent(this,MessageActivity::class.java)
-            intent.putExtra("your_message",message)
-            startActivity(intent)
+        todoAdapter  = CustomAdapter(mutableListOf())  //initialize on create
+        rcView.adapter = todoAdapter
+        rcView.layoutManager = LinearLayoutManager(this)
+        btnAdd.setOnClickListener {
+            val todoTitle  = edtTodo.text.toString()
+            if(todoTitle.isNotEmpty()){
+                val todo = Todo(todoTitle,false)
+                todoAdapter.addTodo(todo)
+                edtTodo.text.clear()
+            }
         }
-        btnShare.setOnClickListener {
-            val message = edtText.text.toString()
-            val intent = Intent()
-            intent.action = Intent.ACTION_SEND
-            intent.putExtra(Intent.EXTRA_TEXT,message)
-            intent.type = "text/plain"
-            startActivity(Intent.createChooser(intent,"Share to : "))
-            /*
-            * lets see
-            * */
-            /*intent.action = Intent.ACTION_DIAL
-            intent.data = Uri.parse("tel:+91$message")
-            startActivity(intent)*/
+        btnDelete.setOnClickListener {
+            todoAdapter.Deletedone()
         }
 
     }
